cc717dd9ad0da9325968107150ccc73b53e44c7d
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-07-22 21:18:04 +0200

    Drafting classes

--- bin/trello_dues_to_google_calendar.dart
+++ bin/trello_dues_to_google_calendar.dart
@@ -7,14 +7,30 @@ import 'package:google_calendar_v3_api/calendar_v3_api_console.dart' as cal;
 import 'package:google_calendar_v3_api/calendar_v3_api_client.dart';
 
 class Trello2Cal {
-  final String id;
-  final String board;
-  final String description;
-  final DateTime due;
-  String id_cal;
-  Trello2Cal(this.id, this.board, this.description, this.due);
-  // TODO
-  // Operators
+  String cardId;
+  String cardDesc;
+  String cardDue;
+  String cardUrl;
+  String boardName;
+  String eventId; // This is set *after* the insertion in Google Calendar
+  Trello2Cal(Map<String, String> card, String boarName) {
+    cardId = card["id"];
+    cardDesc = card["desc"];
+    cardDue = card["due"];
+    cardUrl = card["url"];
+    boardName = boardName;
+  }
+
+  bool operator ==(Trello2Cal other) {
+    return this.cardId == other.cardId && this.cardDesc == other.cardDesc &&
+        this.cardDue == other.cardDue && this.cardUrl == other.cardUrl;
+  }
+
+  // TODO:
+  String toEventJson() {
+    Map<String, dynamic> _eventMap = {};
+    return JSON.encode(_eventMap);
+  }
 }
 
 class Trello2CalSet<E> extends SetBase<E> {
@@ -26,7 +42,7 @@ class Trello2CalSet<E> extends SetBase<E> {
   bool add(E elem) => _set.add(elem);
   bool remove(E elem) => _set.remove(elem);
   E lookup(E elem) => _set.lookup(elem);
-  // Overriding .contains() for fun and profit
+  // Overriding .contains for fun and profit (and to get intersection working!)
   bool contains(E elem) {
     try {
       _set.singleWhere((E e) => e == elem);
@@ -85,17 +101,20 @@ main() {
     log.info("Looking for trello calendar id in Google Calendar");
     calendar.calendarList.list(
         optParams: {"approval_prompt": "auto"}).then((CalendarList list) {
-
+      // FIXME:
+      // type 'CalendarListEntry' is not a subtype of type 'Calendar' in
+      // type cast.
       try {
         trelloCal = list.items.singleWhere((calEntry) {
           return calEntry.summary.toLowerCase() == "trello";
-        }) as Calendar;
+        }) as Calendar; // FIXME: failing type cast
 
       } catch (StateError) {
         log.severe("Trello calendar is not present");
+        log.severe(StateError.toString());
         Calendar calendarRequest = new Calendar.fromJson({"summary": "Trello"});
         calendar.calendars.insert(calendarRequest).then((Calendar cal) {
-          log.fine("Calendario creato con successo");
+          log.fine("Trello Calendar successfully created");
           trelloCal = cal;
         });
       }
@@ -104,7 +123,7 @@ main() {
 }
 
 void error(String msg) {
-  print(msg);
+  print("Error: $msg");
   print("Exiting app.");
   exit(1);
 }
