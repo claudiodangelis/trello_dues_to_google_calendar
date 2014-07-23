732d07836433eb1c965b557d7632f7788514af7c
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-07-23 12:59:36 +0200

    Added retrieving cards algorithm, it works fine

--- bin/trello_dues_to_google_calendar.dart
+++ bin/trello_dues_to_google_calendar.dart
@@ -5,10 +5,13 @@ import 'dart:convert' show JSON;
 import 'package:google_oauth2_client/google_oauth2_console.dart';
 import 'package:google_calendar_v3_api/calendar_v3_api_console.dart' as cal;
 import 'package:google_calendar_v3_api/calendar_v3_api_client.dart';
-
+import 'package:http/http.dart' as http;
 
 final String CONFIG_FILE = "config.json";
 Calendar trelloCal;
+final bool noPastDues = true; // TODO: Move this in config.json
+List<Map<String, dynamic>> _boards;
+List<Map<String, dynamic>> _cards;
 
 main() {
   // TODO: add --debug flag
@@ -101,7 +104,49 @@ main() {
   Trello2CalSet<Trello2Cal> current = getCurrent();
   log.info("Ready to get started retrieving cards");
 
+  // Boards URL
+  String boardsUrl = "https://api.trello.com/1/members/me/boards?" +
+                     "filter=open" + "&" +
+                     "key=" + config["trello_key"] + "&" +
+                     "token=" + config["trello_token"];
+  // TODO:
+  // Gets all the baords
+  http.get(boardsUrl).then((http.Response boardsResp) {
+    // Populating list with all boards
+    _boards = JSON.decode(boardsResp.body);
+    // Getting now all the cards for each board
+    _boards.forEach((Map<String, dynamic> _board) {
+      // Board id (to use to retrieve cards)
+      String _boardId = _board["id"];
+      // Board name (to use to build the Trello2Cal element)
+      String _boardName = _board["name"];
+      // Build the retrive-card string
+      String cardsUrl = "https://api.trello.com/1/boards/$_boardId/cards?" +
+          "card_fields=due" + "&" +
+          "key=" + config["trello_key"] + "&" +
+          "token=" + config["trello_token"];
 
+      // Retrieving all the cards
+      http.get(cardsUrl).then((http.Response cardsResp) {
+        _cards = JSON.decode(cardsResp.body);
+        // Selecting only cards with due date
+        _cards.forEach((Map<String, dynamic> _card) {
+          if (_card["due"] != null) {
+            // Performing check wether it wants or not past dues
+            DateTime now = new DateTime.now();
+            int compare = now.compareTo(DateTime.parse(_card["due"]));
+            if (!noPastDues || (noPastDues && compare.isNegative)) {
+              // This is the card we're looking for
+              log.info("===");
+              log.info(_board["name"]);
+              log.info(_card["name"]);
+              log.info("===");
+            }
+          }
+        });
+      });
+    });
+  });
 }
 
 void error(String msg) {
