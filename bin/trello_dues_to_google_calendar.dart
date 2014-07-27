f787e6d798bc341bd26838d966798fa0d6c6c15d
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-07-27 17:04:55 +0200

    Finished wizard

--- bin/trello_dues_to_google_calendar.dart
+++ bin/trello_dues_to_google_calendar.dart
@@ -1,5 +1,32 @@
 import 'dart:io';
 
-main(List<String> args) {
+import 'package:trello_dues_to_google_calendar/lib.dart';
+import 'package:logging/logging.dart';
+import 'package:args/args.dart';
 
+final Logger log = new Logger('TrelloDues2Calendar');
+
+main(List<String> args) {
+  // Logger configuration
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    print('[${rec.level.name}] ${rec.message}');
+  });
+
+  // Parser configuration
+  ArgParser parser = new ArgParser();
+  parser.addFlag('configure', negatable: false, callback: (configure) {
+    if (configure) {
+      // Start configuration process
+      wizard();
+      print("Finished configuration.\n"
+          "Press <Enter> to run the app (recommended) or CTRL+C to exit.");
+      String _ = stdin.readLineSync();
+
+    }
+  });
+
+  parser.parse(args);
+  print("Starting the app...");
 }
+
--- lib/src/configuration.dart
+++ lib/src/configuration.dart
@@ -1,7 +1,135 @@
 part of trello_dues_to_google_calendar;
 
 final String CONFIG_FILE = 'config.json';
+// TODO: add a logger
+void wizard() {
+  print("Starting configuration wizard. Your current configuration will be deleted. Do you want to go on? [y/n] (default: y)");
+  String _answer = stdin.readLineSync();
 
+  // Default to yes
+  if (_answer == "") {
+    _answer = 'y';
+  }
+
+  if (_answer[0].toLowerCase() == 'y') {
+    // TODO
+    // Yes
+    // Scrivi file conf vuoto
+    createEmptyConfiguration();
+    // Acquisisci trello key
+    print("""
+Please visit
+
+https://trello.com/1/appKey/generate
+
+in order to generate Trello API Key and Secret.
+""");
+
+    print("Insert Trello API Key:");
+    String _trelloApiKey = stdin.readLineSync();
+    // FIXME: add a loop
+    if (_trelloApiKey.trim().isEmpty) {
+      print("Trello Key must not be empty");
+      exit(1);
+    }
+    updateConfiguration("trello_key", _trelloApiKey.trim());
+
+    // Acquisisci trello_secret
+    print("Insert Trello API Secret:");
+    String _trelloApiSecret = stdin.readLineSync();
+    if (_trelloApiSecret.trim().isEmpty) {
+      print("Trello secret must not be empty");
+      exit(1);
+    }
+    updateConfiguration("trello_secret", _trelloApiSecret.trim());
+
+    // Google API
+    // TODO: add more instructions to create an application and get keys
+    print(
+"""
+Good, now you need to generate Google Cloud keys. Please visit
+
+https://cloud.google.com
+
+create a new app, make sure to enable Google Calendar
+APIs. When you're ready paste here the following keys:
+1) Google Client Id
+2) Google Client Secret.
+""");
+
+    // Acquisisci google_client_id
+    print("Insert 'Google Client ID':");
+    String _googleClientId = stdin.readLineSync();
+    if (_googleClientId.trim().isEmpty) {
+      print("Google Client ID must not be empty");
+      exit(1);
+    }
+
+    updateConfiguration("google_client_id", _googleClientId.trim());
+
+    // Acquisisci google_lient_secret
+    print("Insert 'Google Client Secret':");
+    String _googleClientSecret = stdin.readLineSync();
+    if (_googleClientSecret.trim().isEmpty) {
+      print("Google Client Secret must not be empty");
+      exit(1);
+    }
+
+    updateConfiguration("google_client_secret", _googleClientSecret.trim());
+
+    // Creo campi vuoti:
+    // Trello token
+    updateConfiguration("trello_token", "");
+    // id_trello_calendar
+    updateConfiguration("id_trello_calendar", "");
+    // google_credentials
+    updateConfiguration("google_credentials", "google.credentials.json");
+    // google_scopes
+    updateConfiguration("google_scopes",
+        ["https://www.googleapis.com/auth/calendar"]);
+
+
+  } else if (_answer[0].toLowerCase() == 'n') {
+    // No
+    exit(0);
+  } else {
+    // Errore
+    print("Please answer yes or no");
+    exit(1);
+  }
+
+}
+
+String getFilePath(String path) {
+  Directory _parent = new Directory.fromUri(Platform.script).parent;
+  return _parent.path  + Platform.pathSeparator + CONFIG_FILE;
+}
+
+Map<String, dynamic> updateConfiguration(String key, dynamic value) {
+  Map<String, dynamic> _config = readConfiguration();
+  File f = new File(getFilePath(CONFIG_FILE));
+  if (!f.existsSync()) {
+    createEmptyConfiguration();
+  }
+  _config[key] = value;
+  f.writeAsStringSync(JSON.encode(_config), mode: WRITE);
+  return _config;
+}
+
+Map<String, dynamic> readConfiguration() {
+  File f = new File(getFilePath(CONFIG_FILE));
+  if (!f.existsSync()) {
+    createEmptyConfiguration();
+  }
+  return JSON.decode(f.readAsStringSync());
+}
+
+void createEmptyConfiguration() {
+  File f = new File(getFilePath(CONFIG_FILE));
+  f.writeAsStringSync('{}');
+}
+
+/*
 Trello2CalSet<Trello2Cal> getCurrent() {
   // TODO: write this function
   File configFile = new File(CONFIG_FILE);
@@ -62,3 +190,4 @@ String getFilePath(String path) {
   Directory _parent = new Directory.fromUri(Platform.script).parent;
   return _parent.path  + Platform.pathSeparator + CONFIG_FILE;
 }
+*/
